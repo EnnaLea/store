8dfba0dcbbe5cd621557bd67aa5bf9baba230f37
EnnaLea <soleariaeve@gmail.com>
2024-03-12 21:04:00 +0100

    third commit

--- server/server.js
+++ server/server.js
@@ -14,6 +14,54 @@ const stripe = require("stripe")("sk_test_51OtZWTAiWCRGsVDBJGnDW83hfURRAuBP0DtKq
 app.post('/checkout', async(req, res, next) => {
     try {
         const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            shipping_address_collection: {
+            allowed_countries: ['US', 'CA'],
+            },
+                shipping_options: [
+                {
+                    shipping_rate_data: {
+                    type: 'fixed_amount',
+                    fixed_amount: {
+                        amount: 0,
+                        currency: 'eur',
+                    },
+                    display_name: 'Free shipping',
+                    // Delivers between 5-7 business days
+                    delivery_estimate: {
+                        minimum: {
+                        unit: 'business_day',
+                        value: 5,
+                        },
+                        maximum: {
+                        unit: 'business_day',
+                        value: 7,
+                        },
+                    }
+                    }
+                },
+                {
+                    shipping_rate_data: {
+                    type: 'fixed_amount',
+                    fixed_amount: {
+                        amount: 1500,
+                        currency: 'eur',
+                    },
+                    display_name: 'Next day air',
+                    // Delivers in exactly 1 business day
+                    delivery_estimate: {
+                        minimum: {
+                        unit: 'business_day',
+                        value: 1,
+                        },
+                        maximum: {
+                        unit: 'business_day',
+                        value: 1,
+                        },
+                    }
+                    }
+                },
+                ],
             line_items: req.body.items.map((item) => ({
                 price_data: {
                     currency: 'eur',
